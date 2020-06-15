921466202d8f32bf5e5d4b6c03e335cd49def789
Fragamir <35607967+Fragamir@users.noreply.github.com>
2020-06-15 17:21:30 +0100

    Update main.py

--- main.py
+++ main.py
@@ -35,16 +35,15 @@ async def split_send(context, string):
 @client.event
 async def on_ready():
     print(f"\nBot started as: {client.user.name} ({client.user.id}) - Version: {version}")
-    await output_to_test("Bot restarted and ready")
     await client.change_presence(activity=discord.Game("all these hoes"), status=discord.Status.online)
     initial_ext = ["cogs.train", "cogs.music_finder", "cogs.imbd", "cogs.translate", "cogs.owner", "cogs.meme"]
     for ext in initial_ext:
         try:
             client.load_extension(ext)
         except commands.ExtensionFailed as e:
-            await print("Failed to load extension", str(e), ext)
+             print("Failed to load extension", str(e), ext)
         except Exception as e:
-            await print("Failed to do something, unexpected error", str(e), ext)
+            print("Failed to do something, unexpected error", str(e), ext)
 
 
 client.run(os.environ.get("SECRET"))
